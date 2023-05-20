46ec20a5f02d4c5fef6866a221f2a83c2f2ee64d
samratbatra <samratbatra8@gmail.com>
2023-05-20 20:55:00 +0530

    Basic Auth + PasswordEncoder

---
+++ src/test/java/com/burnercode/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burnercode.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
