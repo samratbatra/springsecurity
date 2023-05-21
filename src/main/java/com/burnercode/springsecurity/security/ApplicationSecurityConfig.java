02a99757cfe2794edfbc0c2d12e8f81cd6c74601
samratbatra <samratbatra8@gmail.com>
2023-05-22 03:17:00 +0530

    Permission based authentication

--- src/main/java/com/burnercode/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/burnercode/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.burnercode.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +14,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.burnercode.springsecurity.security.ApplicationUserPermission.*;
 import static com.burnercode.springsecurity.security.ApplicationUserRole.*;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true )
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -28,9 +33,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers(DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,14 +53,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails samratUser = User.builder()
                 .username("Samrat")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) //ROLE_STUDENT
+//                .roles(STUDENT.name()) //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
         UserDetails batraUser = User.builder()
                 .username("Batra")
                 .password(passwordEncoder.encode("password"))
-                .roles(ADMIN.name()) //ROLE_STUDENT
+//                .roles(ADMIN.name()) //ROLE_STUDENT
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+        UserDetails ashokUser = User.builder()
+                .username("Ashok")
+                .password(passwordEncoder.encode("password"))
+//                .roles(ADMINTRAINEE.name()) //ROLE_STUDENT
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
-        return new InMemoryUserDetailsManager(samratUser, batraUser);
+        return new InMemoryUserDetailsManager(samratUser, batraUser, ashokUser);
     }
 }
